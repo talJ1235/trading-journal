f47efe33fb3c109a5df89a77ff59bd2a70520713
Tal1235 <tal.jacoby10@gmail.com>
2026-05-16 04:21:25 +0300

    Fix PWA service worker auto-update on new deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    void updateSW(true)
+  },
+  onOfflineReady() {},
+})
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
